24dbe9736495fdce0d8ccd8c53cb943107835b1b
Rafa Lima <paracafa@gmail.com>
2017-10-09 16:26:38 -0300

    Finalizado as telas de configuração: Account e Theme

--- app/src/main/java/com/example/orafa/androidexercicioviewsfacul/MainActivity.java
+++ app/src/main/java/com/example/orafa/androidexercicioviewsfacul/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.orafa.androidexercicioviewsfacul;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
+    ArrayAdapter<String> mAdapter;
+    List<String> mList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.listViewMenu = (ListView) findViewById(R.id.listViewMenu);
+
+        mList = new ArrayList<>();
+        mList.add(getString(R.string.settings_account));
+        mList.add(getString(R.string.settings_theme));
+
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mList);
+        this.mViewHolder.listViewMenu.setAdapter(mAdapter);
+
+        mViewHolder.listViewMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        Intent itentAccount = new Intent(MainActivity.this, AccountActivity.class);
+                        startActivity(itentAccount);
+                        break;
+                    case 1:
+                        Intent itentTheme = new Intent(MainActivity.this, ThemeActivity.class);
+                        startActivity(itentTheme);
+                        break;
+                }
+
+            }
+        });
+    }
+
+    private static class ViewHolder {
+        ListView listViewMenu;
     }
 }
--- app/src/main/java/com/example/orafa/androidexercicioviewsfacul/ThemeActivity.java
+++ app/src/main/java/com/example/orafa/androidexercicioviewsfacul/ThemeActivity.java
@@ -2,12 +2,36 @@ package com.example.orafa.androidexercicioviewsfacul;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class ThemeActivity extends AppCompatActivity {
+
+    SeekBar customSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theme);
+
+        customSeekBar = (SeekBar) findViewById(R.id.seekBarBrightness);
+
+        //Inicio do SeekBar
+        // perform seek bar change listener event used for getting the progress value
+        customSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //Passar dados para o Text
+            }
+        });
+        //Fim do SeekBar
     }
 }
